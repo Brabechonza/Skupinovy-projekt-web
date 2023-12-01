f8a163ac1c12556a4ffd2361f36debbcd8bb0c15
Brabechonza <134477369+Brabechonza@users.noreply.github.com>
2023-12-01 12:13:56 +0100

    Add files via upload

---
+++ draciovoce.php
@@ -0,0 +1,37 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Dračí ovoce</title>
+            <link rel="stylesheet" href="draciovoce.css">
+        </head>
+    <body>
+        <div class="stred" style="width: 84%;">
+            <h1>Dračí ovoce</h1>
+            <h2>Dragon fruit, dračí ovoce, čínsky 火龍果/龙果 (ohnivý drak) či vietnamsky thanh long (zelený drak), je označení pro plody rodu Hylocereus z čeledi kaktusovitých. Jsou více známé pod názvem pitahaya, zkráceně pitaya, ale tento název je používán i pro mnoho dalších druhů kaktusů, které nespadají pod dračí ovoce a do rodu Hylocereus. Jsou široce pěstované v tropickém pásmu, mimo Ameriku zejména v Asii, kde nejsou původní. Snáší dobře i subtropické pásmo, kde se pěstují v Izraeli[1] a na Kanárských ostrovech.[2] Plody jsou dodávány do obchodních řetězců po celém světě. Výraz pitaya nebo pitahaya pochází z Velkých Antil, z indiánského jazyka Taino a znamená "šupinaté ovoce"[2]. 109 / 5 000</h2>
+        </div>
+
+        <div class="navbar" style="width: 15%;">
+            <h1 id="hltitulek">Ovoce světa</h1>
+            <ul>
+                <li id="skolawebli"><a href="skolaweb.php">Hlavní menu</a></li>
+                <li id="draciovoceli"><a href="draciovoce.php">Dračí ovoce</a></li>
+                <li id="licili"><a href="lici.php">Liči</a></li>
+                <li id="ananasli"><a href="ananas.php">Ananasovník chocholatý</a></li>
+                <li id="bananli"><a href="banan.php">Banán</a></li>
+                <li id="citronli"><a href="citron.php">Citron</a></li>
+            </ul>
+        </div>
+    </body>
+                <body>
+            <div class="napbar" style="width: 15%;">
+            <h1 id="hltitulek">Ovoce světa</h1>
+            <ul>
+                <li id="skolawebli"><a href="skolaweb.css">Hlavní menu</a></li>
+                <li id="draciovoceli"><a href="draciovoce.css">Dračí ovoce</a></li>
+                <li id="licili"><a href="lici.css">Liči</a></li>
+                <li id="ananasli"><a href="ananas.css">Ananasovník chocholatý</a></li>
+                <li id="bananli"><a href="banan.css">Banán</a></li>
+                <li id="citronli"><a href="citron.css">Citron</a></li>
+            </body>
+    </html>
