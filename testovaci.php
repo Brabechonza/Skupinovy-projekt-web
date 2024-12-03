34683894a09eafc0c961d66ced9f36d5f906dfd7
Jakub Čapek <capekj@ad.spskladno.cz>
2024-12-03 11:23:16 +0100

    Prvni pokus

---
+++ testovaci.php
@@ -0,0 +1,37 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Ovoce světa</title>
+            <link rel="stylesheet" href="skolagrafika.css">
+        </head>
+    <body>
+        <div class="stred" style="width: 84%;">
+            <h1>Úvod</h1>
+            <h2>Jakub Čapek, Jakub Kozstolanyi, Honza Brabec</h2>
+        </div>
+
+        <div class="navbar" style="width: 15%;">
+            <h1 id="hltitulek">Ovoce světa</h1>
+            <ul>
+                <li id="skolawebli"><a href="skolaweb.php">Hlavní menu</a></li>
+                <li id="draciovoceli"><a href="draciovoce.php">Dračí ovoce</a></li>
+                <li id="licili"><a href="lici.php">Liči</a></li>
+                <li id="ananasli"><a href="ananas.php">Ananasovník chocholatý</a></li>
+                <li id="bananli"><a href="banan.php">Banán</a></li>
+                <li id="citronli"><a href="citron.php">Citron</a></li>
+            </ul>
+        </div>
+    </body>
+        <body>
+            <div class="napbar" style="width: 15%;">
+            <h1 id="hltitulek">Ovoce světa</h1>
+            <ul>
+                <li id="skolawebli"><a href="skolaweb.css">Hlavní menu</a></li>
+                <li id="draciovoceli"><a href="draciovoce.css">Dračí ovoce</a></li>
+                <li id="licili"><a href="lici.css">Liči</a></li>
+                <li id="ananasli"><a href="ananas.css">Ananasovník chocholatý</a></li>
+                <li id="bananli"><a href="banan.css">Banán</a></li>
+                <li id="citronli"><a href="citron.css">Citron</a></li>
+            </body>
+    </html>
